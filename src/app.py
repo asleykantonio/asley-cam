fdc7a02b4f0782886d3625365c335b1c220e3233
asleykantonio <asleykantonio@gmail.com>
2026-06-05 16:34:23 -0400

    basics for frontend

--- src/app.py
+++ src/app.py
@@ -6,25 +6,41 @@ import camera, layout, printer
 import cv2
 from cv2 import imshow, waitKey
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+## index
 @app.route("/")
 def asley_cam():
-    return "Hello, World!"
+    return render_template("index.html")
 
-# get the frames and store in array frames + frames_names
-frames = []
-frames = camera.capture_frames()
+## ready to take pics page
+@app.route("/capture", methods=["POST"])
+def capture():
+    return render_template("capture.html")
 
-# grab file names in order (0, 1, 2)
-frames_names = sorted(
-    [f for f in os.listdir(".") if f.endswith(".jpg")],
-    key=lambda f: int(re.search(r'\d+', f).group())
-)
+## used to display step by step photostrip
+@app.route("/photostrip/<int:index>", methods=["POST"])
+def photostrip(index):
+    photo = layout.get_photostrip(index)
+    return render_template("capture.html", photo=photo)
 
-# format the photostrip
-strip = layout.strip_layout(frames_names)
+## take pics and display photostrip
+@app.route("/take_pics", methods=["POST"])
+def take_pics():
+    # get the frames and store in array frames + frames_names
+    frames = []
+    frames = camera.capture_frames()
 
-# display the photostrip
-strip.show()
+    # grab file names in order (0, 1, 2)
+    frames_names = sorted(
+        [f for f in os.listdir(".") if f.endswith(".jpg")],
+        key=lambda f: int(re.search(r'\d+', f).group())
+    )
+
+    # format the photostrip
+    strip = layout.strip_layout(frames_names)
+
+    # display the photostrip
+    strip.show()
+
--- src/layout.py
+++ src/layout.py
@@ -6,10 +6,18 @@ def strip_layout(frames_names):
     frame = PIL.Image.open(frames_names[0])
     frame_size = frame.size
     strip = PIL.Image.new("RGB", (frame_size[0]+160, 500+(frame_size[1]*3)), (255, 255, 255)) # blank white image to paste the frames onto
+    print(f"Created blank strip with size: {strip.size}")
 
     for frame_name in frames_names:
         frame = PIL.Image.open(frame_name)
         strip.paste(frame, (80, 80+500*frames_names.index(frame_name))) # paste the frame onto the strip at the correct position
+        photo_name = f"photo_{frames_names.index(frame_name)}.jpg"
+        strip.save(photo_name)
     
     strip.save("photostrip.jpg") # save the photostrip as an image file
     return strip
+
+def get_photostrip(index):
+    photo_name = f"photo_{index}.jpg"
+    photo = PIL.Image.open(photo_name)
+    return photo
